6d65301937e52f91f9cf3f38dfc8d7d1a68796e4
Richard Sutch <richard.sutch@softwire.com>
2021-10-19 15:31:26 +0100

    Added LittlePhotoSelector element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BigPhotoViewer } from './BigPhotoViewer/BigPhotoViewer';
+import { LittlePhotoSelector } from './LittlePhotoSelector/LittlePhotoSelector';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main>
         <h1>Photo Viewer</h1>
         <BigPhotoViewer src="https://picsum.photos/id/102/900/600"></BigPhotoViewer>
+        <LittlePhotoSelector src="https://picsum.photos/seed/a11/900/600"></LittlePhotoSelector>
       </main>
     </div>
   );
---
+++ src/LittlePhotoSelector/LittlePhotoSelector.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './LittlePhotoSelector.css';
+
+export function LittlePhotoSelector(props) {
+    return (
+        <img src={props.src}></img>
+    )
+}
